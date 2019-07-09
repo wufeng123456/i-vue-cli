c4f2f2e9e0d9a929c49c745a09da3a5485e88ec5
wufeng <wufeng@yunshan.net.cn>
2019-07-09 18:20:14 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const program = require('commander')
+const install = require('../bin/install.js')
+program.command('install')
+        .description('install template')
+        .alias('i')
+        .action(() => {
+          console.log('action')
+          install()
+        })
+ 
